60896725ff5937ef46ed5b2a2e9ae4857da30df6
TimothyMoy <Timothy.z.moy@gmail.com>
2020-08-24 15:56:18 -0700

    added post request

--- routes/api/Recipes.js
+++ routes/api/Recipes.js
@@ -3,17 +3,33 @@ const router = express.Router();
 
 // Recipes model
 const Recipes = require('../../models/Recipes');
+const { Recipe } = require('../../config/keys');
 
 //@route GET api/recipes
-//@desc Get ALL Recipes
+//@desc GET ALL Recipes
 //@access Public
 
 router.get('/', (req, res) => {
+  Recipe.find()
+    .sort({ date: -1 })
+    .then(recipe => res.json(recipe))
+
+
+});
+
+//@route POST api/recipes
+//@desc Create a recipe
+//@access Public
+
+router.post('/', (req, res) => {
   const newRecipe = new Recipes({
     name: req.body.name
   });
 
   newRecipe.save().then(recipe =>res.json(recipe))
 });
+
+
+
 
 module.exports = router;
