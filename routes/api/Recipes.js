6838408908e493485d334a6473e4dece3c28098d
TimothyMoy <Timothy.z.moy@gmail.com>
2020-08-24 16:00:30 -0700

    added delete route

--- routes/api/Recipes.js
+++ routes/api/Recipes.js
@@ -29,7 +29,15 @@ router.post('/', (req, res) => {
   newRecipe.save().then(recipe =>res.json(recipe))
 });
 
+//@route DELETE api/recipes/:id
+//@desc Delete a recipe
+//@access Public
 
+router.delete('/:id', (req, res) => {
+  Recipe.findById(req.params.id)
+    .then(recipe => recipe.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}));
+});
 
 
 module.exports = router;
