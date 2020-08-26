dac9a94928155074b78983ae357bfd98d7128e15
TimothyMoy <Timothy.z.moy@gmail.com>
2020-08-26 01:12:17 -0700

    added bootstrap to home and recipe page

--- flavor-app/src/Components/Home/Home.js
+++ flavor-app/src/Components/Home/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Container, Col, Button, Row } from 'reactstrap';
 
 function Home () {
   return(
-    <div>
+    <Container>
+      <Row>
+        <Col xs="6">
       <h1>Create  and Save your favorite recipes.</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at posuere velit. Proin pellentesque neque lacus, sit amet ullamcorper ipsum semper a. Nunc tellus lorem, commodo sit amet metus non, varius scelerisque libero. Curabitur in odio a erat tincidunt tempus. Donec nec lacus pretium, suscipit nulla sit amet, hendrerit orci.</p>
-      <button>
+      <Button>
         <a href="/recipes">
           View Recipes
         </a>
-      </button>
-    </div>
+      </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- flavor-app/src/Components/RecipesList/RecipesList.js
+++ flavor-app/src/Components/RecipesList/RecipesList.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { Container, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Col, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { connect } from 'react-redux';
 import { getRecipes, deleteRecipes } from '../../actions/recipesAction';
@@ -10,21 +11,20 @@ class RecipesList extends Component {
   render() {
     return (
       <Container>
-      <div>
-        New Recipes
         <div>
-          <img src='https://picsum.photos/200' alt='Food'/>
-          <h3>Title</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at posuere velit. Proin pellentesque neque lacus, sit amet ullamcorper ipsum semper a. Nunc tellus lorem, commodo sit amet metus non, varius scelerisque libero. Curabitur in odio a erat tincidunt tempus. Donec nec lacus pretium, suscipit nulla sit amet, hendrerit orci.</p>
-          <button>
-            <a href="/recipe">
-              View Recipe
-            </a>
-          </button>
-        </div>
-       
+          <h1>New Recipes</h1>
+          <Col xs="3">
+            <Card>
+              <CardImg top width="10%" src="https://picsum.photos/100" alt="Food image cap" />
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                <Button><a href="/recipe">View Recipe</a></Button>
+              </CardBody>
+            </Card>
+          </Col>
       </div>
-        
       </Container>
     )
   }
