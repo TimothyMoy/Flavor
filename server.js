2c5347dbb3c77905db179a2c4480b75d6381d8ee
TimothyMoy <Timothy.z.moy@gmail.com>
2020-08-27 14:47:53 -0700

    heroku deploy test 2

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
 // DB Config
 const db = config.get("mongoURI");
@@ -28,14 +29,6 @@ app.use('/api/Recipes',require('./routes/api/Recipes'));
 app.use('/api/Users', require('./routes/api/Users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-// Server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-} 
 
 const PORT = process.env.PORT || 4000;
 
